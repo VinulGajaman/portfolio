48742a386bcc2e5e4f769b801096009559ce2195
Vinul <vinulgajaman2001@gmail.com>
2025-05-13 21:02:06 +0530

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Alias '@' points to 'src' folder
